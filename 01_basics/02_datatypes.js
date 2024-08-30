b996028dbb4b9e14cdd3a2454a3fec1c04196d53
Amit11001 <choudharyamit564@gmail.com>
2024-08-30 11:39:11 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we use nodejs, not browser
+
+
+console.log(3+3) // code readability should be high
+
+console.log("Amit")
+
+let name ="Amit"
+let age = 21
+let isloggedIn = false
+let state;
+
+// numbr => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique    
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
